cb30279bd0d8af39de6856dc8459d58551a2d680
OlenaOP <elena.peredriy@gmail.com>
2023-05-19 00:50:15 +0300

    add tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const makeGalleryCard = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+const markup = galleryItems.map(element => makeGalleryCard(element)).join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+let galleryItem = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galleryItem.next();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const CURRENT_FORM_STATE = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+// const message = document.querySelector('message');
+// const button = document.querySelector('button');
+
+let formState = JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)) || {};
+
+form.addEventListener('input', throttle(onTextInput, 500));
+
+function onTextInput(evt) {
+  formState[evt.target.name] = evt.target.value;
+  const stringData = JSON.stringify(formState);
+  localStorage.setItem(CURRENT_FORM_STATE, stringData);
+  //localStorage.setItem(CURRENT_FORM_STATE, JSON.stringify(formState));
+}
+
+//const form = document.querySelector('#message-form');
+//const output = document.querySelector('#output');
+//const LOCALSTORAGE_KEY = 'goit-example-message';
+//form.addEventListener('input', throttle(updateOutput, 500));
+updateOutput();
+form.addEventListener('submit', saveMessage);
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  console.log('formState', formState);
+  formState.email = form.elements.email.value;
+  formState.message = form.elements.message.value;
+  console.log('formState.message', formState.message);
+  console.log('form.elements.message', form.elements.message);
+  localStorage.removeItem(CURRENT_FORM_STATE);
+  formState = {};
+  updateOutput();
+  form.reset();
+}
+
+function updateOutput() {
+  form.elements.email.value =
+    JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)).email || '';
+  form.elements.message.textContent =
+    JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)).message || '';
+}
