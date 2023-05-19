442856fdae07e3f054dec3b1fe36694cfbe605f4
OlenaOP <elena.peredriy@gmail.com>
2023-05-19 11:19:10 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,34 +12,29 @@ form.addEventListener('input', throttle(onTextInput, 500));
 
 function onTextInput(evt) {
   formState[evt.target.name] = evt.target.value;
-  const stringData = JSON.stringify(formState);
-  localStorage.setItem(CURRENT_FORM_STATE, stringData);
-  //localStorage.setItem(CURRENT_FORM_STATE, JSON.stringify(formState));
+  localStorage.setItem(CURRENT_FORM_STATE, JSON.stringify(formState));
 }
 
-//const form = document.querySelector('#message-form');
-//const output = document.querySelector('#output');
-//const LOCALSTORAGE_KEY = 'goit-example-message';
 //form.addEventListener('input', throttle(updateOutput, 500));
 updateOutput();
 form.addEventListener('submit', saveMessage);
 
 function saveMessage(evt) {
   evt.preventDefault();
-  console.log('formState', formState);
   formState.email = form.elements.email.value;
   formState.message = form.elements.message.value;
-  console.log('formState.message', formState.message);
-  console.log('form.elements.message', form.elements.message);
+  console.log('formState', formState);
+  //console.log('formState.message =', formState.message);
+  //console.log('form.elements.message =', form.elements.message);
+  updateOutput();
   localStorage.removeItem(CURRENT_FORM_STATE);
   formState = {};
-  //updateOutput();
   form.reset();
 }
 
 function updateOutput() {
   form.elements.email.value =
     JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)).email || '';
-  form.elements.message.textContent =
+  form.elements.message.value =
     JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)).message || '';
 }
